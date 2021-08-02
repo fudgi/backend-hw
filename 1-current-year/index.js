63f434e2afee316d6f4b4e62e56e3814ad2df7dc
Андрей Сельдюков <a.seldukov@onpoint.ru>
2021-08-02 19:33:45 +0300

    1-current

--- 1-current-year/index.js
+++ 1-current-year/index.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
-const { checkKeys, gatherArgs } = require("./utils.js");
+const {
+  checkKeys,
+  gatherArgs,
+  add,
+  sub,
+  calculateData,
+  logDate,
+} = require("./utils.js");
 
 const Arg = {
   year: ["year", "y"],
@@ -26,35 +33,8 @@ const yearKey = checkKeys(argv, Arg.year);
 const monthKey = checkKeys(argv, Arg.month);
 const dateKey = checkKeys(argv, Arg.date);
 
-const add = (a, b) => a + b;
-const sub = (a, b) => a - b;
+const keys = [yearKey, monthKey, dateKey];
 
-const calculateData = (func) => {
-  const date = new Date();
-  if (yearKey) {
-    const fYear = date.getFullYear();
-    date.setFullYear(func(fYear, argv[yearKey]));
-  }
-  if (monthKey) {
-    const fMonth = date.getMonth();
-    date.setMonth(func(fMonth, argv[monthKey]));
-  }
-  if (dateKey) {
-    const fDate = date.getDate();
-    date.setDate(func(fDate, argv[dateKey]));
-  }
-  console.log(date);
-};
-
-if (isAddArgs) calculateData(add);
-else if (isSubArgs) calculateData(sub);
-else {
-  const date = new Date();
-  const answer = [];
-
-  if (yearKey) answer.push(date.getUTCFullYear());
-  if (monthKey) answer.push(date.getUTCMonth() + 1);
-  if (dateKey) answer.push(date.getUTCDate());
-
-  console.log(answer.join(" "));
-}
+if (isAddArgs) calculateData(add, argv, keys);
+else if (isSubArgs) calculateData(sub, argv, keys);
+else logDate(keys);
--- 1-current-year/utils.js
+++ 1-current-year/utils.js
@@ -10,7 +10,45 @@ const gatherArgs = (obj) =>
     return acc;
   }, []);
 
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+const calculateData = (func, argv, keys) => {
+  const [yearKey, monthKey, dateKey] = keys;
+  const date = new Date();
+
+  if (yearKey) {
+    const fYear = date.getFullYear();
+    date.setFullYear(func(fYear, argv[yearKey]));
+  }
+  if (monthKey) {
+    const fMonth = date.getMonth();
+    date.setMonth(func(fMonth, argv[monthKey]));
+  }
+  if (dateKey) {
+    const fDate = date.getDate();
+    date.setDate(func(fDate, argv[dateKey]));
+  }
+  console.log(date);
+};
+
+const logDate = (keys) => {
+  const [yearKey, monthKey, dateKey] = keys;
+  const date = new Date();
+  const answer = [];
+
+  if (yearKey) answer.push(date.getUTCFullYear());
+  if (monthKey) answer.push(date.getUTCMonth() + 1);
+  if (dateKey) answer.push(date.getUTCDate());
+
+  console.log(answer.join(" "));
+};
+
 module.exports = {
   checkKeys,
-  gatherArgs
+  gatherArgs,
+  add,
+  sub,
+  calculateData,
+  logDate,
 };
