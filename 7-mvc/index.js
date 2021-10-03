44d7ba47ed5670a172cb6b0cb7c1619fef83235c
Андрей Сельдюков <a.seldukov@onpoint.ru>
2021-10-03 21:42:24 +0300

    7-mvc: pug views

--- 7-mvc/index.js
+++ 7-mvc/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+app.use(express.static("public"));
 app.set('views', path.join(__dirname, 'src/views'))
 app.set("view engine", "pug")
 
@@ -12,6 +13,14 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
     res.render('books')
+})
+
+app.get('/add', (req, res) => {
+    res.render('add')
+})
+
+app.get('/view', (req, res) => {
+    res.render('view')
 })
 
 app.listen(3000)
